8e01b4b4c84858f2111e334040bf476f51b59e0a
JerryWuOn <Jon4@my.bcit.ca>
2024-09-05 15:41:55 -0700

    created homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,64 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ExampleImage from "../app/images/ExamplePhoto.png"
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <header className={styles.header}>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+        <h1 className={styles.headingTitle}>Lonsdale Bike</h1>
+        <nav className={styles.nav}>
+          <div className={styles.ctas}>
+            <a className={styles.primary} href="/https://www.facebook.com/" target="_blank"rel="noopener noreferrer">
+              Facebook Page
+            </a>
+          </div>
+        </nav>
+      </header>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <div className={styles.introduction}>
+            <h3 className={styles.heading}>Check out our</h3>
+            <h1 className={styles.mainHeading}>Lonsdale Bike </h1>
+            <h3 className={styles.heading}>Adventure awaits on two wheels! Join us for the ride and explore the world one pedal at a time. 🚴‍♂️</h3>
+          </div>
+          <div className={styles.ctas}>
+            <a
+              className={styles.primary}
+              href="/https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              FaceBook Page
+            </a>
+          </div>
+        </main>
+      </div>
+
+      <div className={styles.introductionSection}>
+        <h1 className={styles.introductionHeader}>See how we did it</h1>
+        <main className={styles.gridContainer}>
+          <div className={styles.gridItem}>
+            <Image src={ExampleImage} alt="Image 1" className={styles.image} />
+            <h2 className={styles.title}>Events</h2>
+          </div>
+          <div className={styles.gridItem}>
+            <Image src={ExampleImage} alt="Image 2" className={styles.image} />
+            <h2 className={styles.title}>Exploration</h2>
+          </div>
+          <div className={styles.gridItem}>
+            <Image src={ExampleImage} alt="Image 3" className={styles.image} />
+            <h2 className={styles.title}>Destinations</h2>
+          </div>
+        </main>
+      </div>
+
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div className={styles.footerSection}>
+          <h2 className={styles.footerTitle}>Stay Connected. All rights Reserved</h2>
+        </div>
       </footer>
     </div>
   );
